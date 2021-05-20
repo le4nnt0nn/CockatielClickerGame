8b8643aa4f87b17f7a11da032b7bfc02249bbe71
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-20 19:42:39 +0200

    js clicks : changed buy to check_buy

--- js/clicks.js
+++ js/clicks.js
@@ -87,8 +87,8 @@ function reset() {
     highscore_count.innerHTML = highscore;
 }
 
-function buy() {
-    var buy1 = document.getElementById("buy_button");
+function check_buy() {
+    var buy1 = document.getElementById("buy_button1");
     if(clicks>=50){
         buy1.style.display = 'inline';
     } else {
