cb9a5c2f9dc9518a7c6d8cfb80524a8c01c4a146
Antonio <ditacj78@gmail.com>
2021-05-20 19:50:56 +0200

    js clicks : deleted check_buy

--- js/clicks.js
+++ js/clicks.js
@@ -87,11 +87,4 @@ function reset() {
     highscore_count.innerHTML = highscore;
 }
 
-function check_buy() {
-    var buy1 = document.getElementById("buy_button1");
-    if(clicks>=50){
-        buy1.style.display = 'inline';
-    } else {
-        buy1.style.display = 'none';
-    }
-}
+
