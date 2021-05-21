8b5aacfa0860446073b8cfb3adfa724e05605aa5
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-21 16:22:17 +0200

    js clicks : beautify implemented

--- js/clicks.js
+++ js/clicks.js
@@ -1,41 +1,39 @@
-//Vars
+// Vars
 var clicks = 0;
 var score = 0;
 var highscore = 0;
 
-//Function
+// Increment
 function increment() {
-    
-    clicks = clicks + 1;
-    seed_counter.innerHTML = clicks;
-    if (buy1_count>0) {
-        clicks = clicks + buy1_count + 2;
-    }
+  clicks = clicks + 1;
+  seed_counter.innerHTML = clicks;
+  if (buy1_count > 0) {
+    clicks = clicks + buy1_count + 2;
+  }
 
-    if (buy2_count>0) {
-        clicks = clicks + buy2_count + 10;
-    }
+  if (buy2_count > 0) {
+    clicks = clicks + buy2_count + 10;
+  }
 
-    if (buy3_count>0) {
-        clicks = clicks + buy3_count + 100;
-    }
+  if (buy3_count > 0) {
+    clicks = clicks + buy3_count + 100;
+  }
 
-    if (buy4_count>0) {
-        clicks = clicks + buy4_count + 1000;
-    }
-    
+  if (buy4_count > 0) {
+    clicks = clicks + buy4_count + 1000;
+  }
 
-    //Score
-    score = clicks;
-    if (score > highscore) {
-        highscore = score;    
-    }
+  // Score
+  score = clicks;
+  if (score > highscore) {
+    highscore = score;
+  }
 }
+
+// Reset function
 function reset() {
-    clicks = 0;
-    seed_counter.innerHTML = clicks;
-    //Save Highscore
-    highscore_count.innerHTML = highscore;
+  clicks = 0;
+  seed_counter.innerHTML = clicks;
+  //Save Highscore
+  highscore_count.innerHTML = highscore;
 }
-
-
