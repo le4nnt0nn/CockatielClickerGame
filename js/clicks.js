d41065daa17f7c00efbacef9fac34f23960819a0
Antonio <ditacj78@gmail.com>
2021-05-21 11:55:47 +0200

    js clicks : changed structure

--- js/clicks.js
+++ js/clicks.js
@@ -6,17 +6,22 @@ var highscore = 0;
 //Function
 function increment() {
     
+    clicks = clicks + 1;
     seed_counter.innerHTML = clicks;
     if (buy1_count>0) {
         clicks = clicks + buy1_count + 2;
-    } else {
-        clicks = clicks + 1;
     }
 
     if (buy2_count>0) {
-        clicks = clicks + buy1_count + 10;
-    } else {
-        clicks = clicks + 1;
+        clicks = clicks + buy2_count + 10;
+    }
+
+    if (buy3_count>0) {
+        clicks = clicks + buy3_count + 100;
+    }
+
+    if (buy4_count>0) {
+        clicks = clicks + buy4_count + 1000;
     }
     
 
